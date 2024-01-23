f5c800ef80a3b784929696d518b7bbe829950550
Gabor Kovacs <gabor.kovacs@dakai.io>
2024-01-23 16:02:47 +0100

    refactor: restructure files

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -1,9 +1,6 @@
-package main
+package httpserver
 
 import (
-	//"context"
-	//"encoding/binary"
-	//"errors"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -13,63 +10,11 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	//"bytes"
-
-	"github.com/gabkov/krnl-node/rpc"
 )
-
-type testService struct{}
-
-type echoArgs struct {
-	S string
-}
-
-type echoResult struct {
-	String string
-	Int    int
-	Args   *echoArgs
-}
-
-func (s *testService) Null() any {
-	return nil
-}
-
-func (s *testService) Echo(str string, i int, args *echoArgs) echoResult {
-	return echoResult{str, i, args}
-}
-
-type notificationTestService struct {
-	unsubscribed            chan string
-	gotHangSubscriptionReq  chan struct{}
-	unblockHangSubscription chan struct{}
-}
-
-func (s *notificationTestService) Echo(i int) int {
-	return i
-}
-
-func (s *notificationTestService) Unsubscribe(subid string) {
-	if s.unsubscribed != nil {
-		s.unsubscribed <- subid
-	}
-}
-
-func newTestServer() *rpc.Server {
-	server := rpc.NewServer()
-	//server.idgen = sequentialIDGenerator()
-	if err := server.RegisterName("test", new(testService)); err != nil {
-		panic(err)
-	}
-	if err := server.RegisterName("nftest", new(notificationTestService)); err != nil {
-		panic(err)
-	}
-	return server
-}
 
 // A Server is an HTTP server listening on a system-chosen port on the
 // local loopback interface, for use in end-to-end HTTP tests.
-type Server struct {
+type HttpServer struct {
 	URL      string // base URL of form http://ipaddr:port with no trailing slash
 	Listener net.Listener
 
@@ -105,7 +50,7 @@ type Server struct {
 
 // NewServer starts and returns a new Server.
 // The caller should call Close when finished, to shut it down.
-func NewServer(handler http.Handler) *Server {
+func NewHttpServer(handler http.Handler) *HttpServer {
 	ts := NewUnstartedServer(handler)
 	ts.Start()
 	return ts
@@ -117,32 +62,14 @@ func NewServer(handler http.Handler) *Server {
 // StartTLS.
 //
 // The caller should call Close when finished, to shut it down.
-func NewUnstartedServer(handler http.Handler) *Server {
-	return &Server{
+func NewUnstartedServer(handler http.Handler) *HttpServer {
+	return &HttpServer{
 		Listener: newLocalListener(),
 		Config:   &http.Server{Handler: handler},
 	}
 }
 
-// When debugging a particular http server-based test,
-// this flag lets you run
-//
-//	go test -run=BrokenTest -httptest.serve=127.0.0.1:8000
-//
-// to start the broken server so you can interact with it manually.
-// We only register this flag if it looks like the caller knows about it
-// and is trying to use it as we don't want to pollute flags and this
-// isn't really part of our API. Don't depend on this.
-var serveFlag string
-
 func newLocalListener() net.Listener {
-	if serveFlag != "" {
-		l, err := net.Listen("tcp", serveFlag)
-		if err != nil {
-			panic(fmt.Sprintf("httptest: failed to listen on %v: %v", serveFlag, err))
-		}
-		return l
-	}
 	l, err := net.Listen("tcp", "127.0.0.1:1234")
 	if err != nil {
 		if l, err = net.Listen("tcp6", "[::1]:0"); err != nil {
@@ -153,7 +80,7 @@ func newLocalListener() net.Listener {
 }
 
 // Start starts a server from NewUnstartedServer.
-func (s *Server) Start() {
+func (s *HttpServer) Start() {
 	if s.URL != "" {
 		panic("Server already started")
 	}
@@ -165,17 +92,15 @@ func (s *Server) Start() {
 	s.goServe()
 }
 
-func (s *Server) goServe() {
+func (s *HttpServer) goServe() {
 	s.wg.Add(1)
-	//go func() {
 	defer s.wg.Done()
 	s.Config.Serve(s.Listener)
-	//}()
 }
 
 // wrap installs the connection state-tracking hook to know which
 // connections are idle.
-func (s *Server) wrap() {
+func (s *HttpServer) wrap() {
 	oldHook := s.Config.ConnState
 	s.Config.ConnState = func(c net.Conn, cs http.ConnState) {
 		s.mu.Lock()
@@ -235,11 +160,11 @@ func (s *Server) wrap() {
 
 // closeConn closes c.
 // s.mu must be held.
-func (s *Server) closeConn(c net.Conn) { s.closeConnChan(c, nil) }
+func (s *HttpServer) closeConn(c net.Conn) { s.closeConnChan(c, nil) }
 
 // closeConnChan is like closeConn, but takes an optional channel to receive a value
 // when the goroutine closing c is done.
-func (s *Server) closeConnChan(c net.Conn, done chan<- struct{}) {
+func (s *HttpServer) closeConnChan(c net.Conn, done chan<- struct{}) {
 	c.Close()
 	if done != nil {
 		done <- struct{}{}
@@ -248,7 +173,7 @@ func (s *Server) closeConnChan(c net.Conn, done chan<- struct{}) {
 
 // Close shuts down the server and blocks until all outstanding
 // requests on this server have completed.
-func (s *Server) Close() {
+func (s *HttpServer) Close() {
 	s.mu.Lock()
 	if !s.closed {
 		s.closed = true
@@ -300,7 +225,7 @@ func (s *Server) Close() {
 	s.wg.Wait()
 }
 
-func (s *Server) logCloseHangDebugInfo() {
+func (s *HttpServer) logCloseHangDebugInfo() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	var buf strings.Builder
@@ -313,56 +238,4 @@ func (s *Server) logCloseHangDebugInfo() {
 
 type closeIdleTransport interface {
 	CloseIdleConnections()
-}
-
-func main() {
-
-	srv := newTestServer()
-	NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		srv.ServeHTTP(w, r)
-	}))
-	//defer httpsrv.Close()
-	//defer srv.Stop()
-
-	// server := newTestServer()
-	// //defer server.Stop()
-
-	// listener, err := net.Listen("tcp", "127.0.0.1:1234")
-	// if err != nil {
-	// 	log.Fatal("can't listen:", err)
-	// }
-	// log.Println(listener.Addr())
-	// //defer listener.Close()
-	// server.ServeListener(listener)
-
-	// var (
-	// 	request  = `{"jsonrpc":"2.0","id":1,"method":"rpc_modules"}` + "\n"
-	// 	wantResp = `{"jsonrpc":"2.0","id":1,"result":{"nftest":"1.0","rpc":"1.0","test":"1.0"}}` + "\n"
-	// 	deadline = time.Now().Add(10 * time.Second)
-	// )
-
-	// for i := 0; i < 20; i++ {
-	// 	conn, err := net.Dial("tcp", listener.Addr().String())
-	// 	if err != nil {
-	// 		log.Fatal("can't dial:", err)
-	// 	}
-
-	// 	conn.SetDeadline(deadline)
-	// 	// Write the request, then half-close the connection so the server stops reading.
-	// 	conn.Write([]byte(request))
-	// 	conn.(*net.TCPConn).CloseWrite()
-	// 	// Now try to get the response.
-	// 	buf := make([]byte, 2000)
-	// 	n, err := conn.Read(buf)
-	// 	conn.Close()
-
-	// 	if err != nil {
-	// 		log.Fatal("read error:", err)
-	// 	}
-	// 	//log.Println("LOL")
-	// 	//log.Println(buf[:n])
-	// 	if !bytes.Equal(buf[:n], []byte(wantResp)) {
-	// 		log.Fatalf("wrong response: %s", buf[:n])
-	// 	}
-	// }
 }
--- main.go
+++ main.go
@@ -1,187 +1,62 @@
-// package main
+package main
 
-// import (
-// 	"bytes"
-// 	"context"
-// 	"encoding/hex"
-// 	"encoding/json"
-// 	"errors"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"net/http"
-// 	"strings"
+import (
+	"log"
+	"net/http"
+	"github.com/gabkov/krnl-node/rpc"
+	hs "github.com/gabkov/krnl-node/httpserver"
+)
 
-// 	"github.com/ethereum/go-ethereum/common/hexutil"
-// 	"github.com/ethereum/go-ethereum/core/types"
-// 	"github.com/ethereum/go-ethereum/ethclient"
-// 	"github.com/ethereum/go-ethereum/rlp"
-// 	"github.com/gorilla/rpc"
-// 	gjson "github.com/gorilla/rpc/json"
-// )
+type testService struct{}
 
-// type RegisterDapp struct {
-// 	DappName string `json:"dappName" binding:"required"`
-// }
+type echoArgs struct {
+	S string
+}
 
-// type RegisteredDapp struct {
-// 	AccessToken             string `json:"accessToken" binding:"required"`
-// 	TokenAuthorityPublicKey string `json:"tokenAuthorityPublicKey" binding:"required"`
-// }
+type echoResult struct {
+	String string
+	Int    int
+	Args   *echoArgs
+}
 
-// type TxRequest struct {
-// 	DappName    string `json:"dappName" binding:"required"`
-// 	AccessToken string `json:"accessToken" binding:"required"`
-// 	Message     string `json:"message" binding:"required"`
-// }
+func (s *testService) Null() any {
+	log.Println("kis faszocska")
+	return nil
+}
 
-// type SignatureToken struct {
-// 	SignatureToken string `json:"signatureToken" binding:"required"`
-// }
+func (s *testService) Echo(str string, i int, args *echoArgs) echoResult {
+	return echoResult{str, i, args}
+}
 
-// type RawTransaction struct {
-// 	RawTx string `json:"rawTx" binding:"required"`
-// }
+type notificationTestService struct {
+	unsubscribed            chan string
+	gotHangSubscriptionReq  chan struct{}
+	unblockHangSubscription chan struct{}
+}
 
-// type TransactionHash struct {
-// 	TxHash string `json:"txHash" binding:"required"`
-// }
+func (s *notificationTestService) Echo(i int) int {
+	return i
+}
 
-// type KrnlTask struct{}
+func (s *notificationTestService) Unsubscribe(subid string) {
+	if s.unsubscribed != nil {
+		s.unsubscribed <- subid
+	}
+}
 
-// const TOKEN_AUTHORITY = "http://localhost:8080" // TODO: env
 
-// func main() {
-// 	rpcServer := rpc.NewServer()
-
-// 	rpcServer.RegisterCodec(gjson.NewCodec(), "application/json")
-
-// 	rpcServer.RegisterService(new(KrnlTask), "")
-
-// 	http.Handle("/krnl", rpcServer)
-
-// 	log.Printf("Serving RPC server on port %d", 1337)
-
-// 	http.ListenAndServe("localhost:1337", nil)
-// }
-
-// func (t *KrnlTask) RegisterNewDapp(r *http.Request, registerDapp *RegisterDapp, reply *RegisteredDapp) error {
-// 	log.Println("RegisterNewDapp called")
-// 	registerDappPayload, err := json.Marshal(registerDapp)
-// 	if err != nil {
-// 		fmt.Println("Error marshalling JSON:", err)
-// 		return nil
-// 	}
-
-// 	body := callTokenAuthority("/register-dapp", registerDappPayload)
-// 	if body == nil {
-// 		return errors.New("Transaction rejected: invalid access token")
-// 	}
-
-// 	var registeredDapp RegisteredDapp
-// 	err = json.Unmarshal(body, &registeredDapp)
-// 	if err != nil {
-// 		fmt.Println("error unmarshalling response JSON:", err)
-// 		return nil
-// 	}
-
-// 	*reply = registeredDapp
-// 	return nil
-// }
-
-// func (t *KrnlTask) TxRequest(r *http.Request, txRequest *TxRequest, reply *SignatureToken) error {
-// 	log.Println("TxRequest called")
-// 	txRequestPayload, err := json.Marshal(txRequest)
-// 	if err != nil {
-// 		fmt.Println("Error marshalling JSON:", err)
-// 		return nil
-// 	}
-
-// 	body := callTokenAuthority("/tx-request", txRequestPayload)
-// 	if body == nil {
-// 		return errors.New("Transaction rejected: invalid access token")
-// 	}
-
-// 	var signatureToken SignatureToken
-// 	err = json.Unmarshal(body, &signatureToken)
-// 	if err != nil {
-// 		fmt.Println("error unmarshalling response JSON:", err)
-// 		return nil
-// 	}
-
-// 	*reply = signatureToken
-// 	return nil
-// }
-
-// func (t *KrnlTask) SendTx(r *http.Request, rawTx *RawTransaction, reply *TransactionHash) error {
-// 	log.Println("SendTx called")
-
-// 	client, err := ethclient.Dial("http://127.0.0.1:8545") // TODO: env or parameter
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	rawTxBytes, err := hex.DecodeString(rawTx.RawTx[2:])
-
-// 	tx := new(types.Transaction)
-// 	err = rlp.DecodeBytes(rawTxBytes, &tx)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// Simulate stopping tx here
-// 	// grabbing the requested FaaS services from the end of the input-data
-// 	separator := "000000000000000000000000000000000000000000000000000000000000003a" // :
-// 	res := strings.Split(hexutil.Encode(tx.Data()), separator)
-
-// 	// if len is more than 1 some message is concatenated to the end of the input-data
-// 	if len(res) > 1 {
-// 		faas, err := hex.DecodeString(res[1]) // TODO: handle multiple msg
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		log.Println("Requested FaaS:", string(faas))
-// 		// do the Faas here ...
-// 	}
-
-// 	err = client.SendTransaction(context.Background(), tx)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("tx sent: %s", tx.Hash().Hex())
-
-// 	*reply = TransactionHash{TxHash: tx.Hash().Hex()}
-// 	return nil
-// }
-
-// func callTokenAuthority(path string, payload []byte) []byte {
-// 	req, err := http.NewRequest("POST", TOKEN_AUTHORITY+path, bytes.NewBuffer(payload))
-// 	if err != nil {
-// 		fmt.Println("Error creating request:", err)
-// 		return nil
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		fmt.Println("Error sending request:", err)
-// 		return nil
-// 	}
-// 	defer resp.Body.Close()
-
-// 	if resp.StatusCode == 401 {
-// 		return nil
-// 	}
-
-// 	// Read the response body
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println("Error reading response body:", err)
-// 		return nil
-// 	}
-
-// 	return body
-// }
+func main() {
+	srv := rpc.NewServer()
+	if err := srv.RegisterName("test", new(testService)); err != nil {
+		panic(err)
+	}
+	if err := srv.RegisterName("nftest", new(notificationTestService)); err != nil {
+		panic(err)
+	}
+	
+	httpsrv := hs.NewHttpServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		srv.ServeHTTP(w, r)
+	}))
+	defer httpsrv.Close()
+	defer srv.Stop()
+}
