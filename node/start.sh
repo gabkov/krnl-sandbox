d0e5924f775efbdf8cd7548b01e00ddbcf3cc19a
StRane <umutseber4@gmail.com>
2024-02-11 12:41:05 +0700

    my local system change, need to docker exec start.sh first before using

--- node/start.sh
+++ node/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
 screen -d -m npx hardhat node & 
-P1=$!
+echo "Hardhat node installing"
+# P1=$!
 ./krnl_node &
-P2=$!
-wait $P1 $P2
+echo "Krnl node installing"
+# P2=$!
+# wait $P1 $P2
